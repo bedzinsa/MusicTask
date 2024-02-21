04ccbc3720b62844fa5bc18debbe958a05c2e4e3
Arunas Bedzinskas <a.bedzinskas@gmail.com>
2024-02-21 19:36:37 +0200

    Removed animation of top bar - clashing with NavHost, looks bad

--- app/src/main/java/com/arunasbedzinskas/musictask/main/MainActivity.kt
+++ app/src/main/java/com/arunasbedzinskas/musictask/main/MainActivity.kt
@@ -3,9 +3,6 @@ package com.arunasbedzinskas.musictask.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -16,9 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.arunasbedzinskas.musictask.EMPTY_STRING
+import com.arunasbedzinskas.musictask.R
 import com.arunasbedzinskas.musictask.navigation.AppNavigation
 import com.arunasbedzinskas.musictask.ui.theme.AppTheme
 
@@ -31,38 +29,29 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun Main() {
-        val topBarTitleState = rememberSaveable { mutableStateOf(EMPTY_STRING) }
+        val appName = stringResource(R.string.app_name)
+        val topBarTitleState = rememberSaveable { mutableStateOf(appName) }
         val navController = rememberNavController()
         val navGraph = AppNavigation.getGraph(navController, topBarTitleState)
 
-        Scaffold(
-            topBar = { TopBar(topBarTitleState.value) },
-            content = { paddingValues ->
-                NavHost(
-                    navController = navController,
-                    graph = navGraph,
-                    modifier = Modifier.padding(paddingValues)
-                )
-            }
-        )
+        Scaffold(topBar = { TopBar(topBarTitleState.value) }, content = { paddingValues ->
+            NavHost(
+                navController = navController,
+                graph = navGraph,
+                modifier = Modifier.padding(paddingValues)
+            )
+        })
     }
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun TopBar(title: String) {
-        AnimatedVisibility(
-            visible = title.isNotBlank(),
-            enter = slideInVertically { -it },
-            exit = slideOutVertically { -it }
-        ) {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.headlineMedium
-                    )
-                }
-            )
-        }
+        TopAppBar(
+            title = {
+                Text(
+                    text = title, style = MaterialTheme.typography.headlineMedium
+                )
+            },
+        )
     }
 }
